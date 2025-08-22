4f8b7cb0de6b17b024cfe16962c219675b9da398
Jay Chew <jay.chew@mediacliq.my>
2025-08-23 01:41:21 +0800

    update changes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,11 +10,6 @@ export const metadata: Metadata = {
   title: 'Dashboard',
 };
 
-// 🚀 强制动态渲染，避免 build 阶段连数据库
-export const dynamic = "force-dynamic";
-// 或者
-export const revalidate = 0;
-
 export default async function Page() {
 
   return (
